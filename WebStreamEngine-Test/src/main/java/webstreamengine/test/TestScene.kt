894452c5d5c5f369f8b8680148fb0d5aaa968cfb
DaylightNebula <daylightnebula1@gmail.com>
2023-03-23 13:32:47 -0600

    Updated gitignore

--- WebStreamEngine-Test/src/main/java/webstreamengine/test/TestScene.kt
+++ WebStreamEngine-Test/src/main/java/webstreamengine/test/TestScene.kt
@@ -1,10 +1,6 @@
 package webstreamengine.test
 
-import org.json.JSONObject
-import webstreamengine.client.entities.Entity
-import webstreamengine.client.networking.Connection
 import webstreamengine.client.scenes.Scene
-import java.util.*
 
 class TestScene: Scene("test_scene") {
     override fun generalStart() {
@@ -16,16 +12,4 @@ class TestScene: Scene("test_scene") {
 
     override fun generalStop() {
     }
-
-    override fun netJoin(conn: Connection) {
-        Entity.createFromPath(UUID.randomUUID(), false, "player") {
-            it.assignedTo = conn.id
-        }
-    }
-
-    override fun netDisconnect(conn: Connection) {
-
-    }
-
-    override fun netCommunicate(json: JSONObject) {}
 }
